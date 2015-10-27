bb05ad7102480ab071117eadb4ea806f43cccb60
wolf <wolf@pengyandeMacBook-Pro.local>
2015-10-27 08:01:28 +0800

    add xmind, update ppt, update demo

---
+++ AnimationSkillShare/MyAnimationDemo/app/src/main/java/com/example/wolf/myanimationdemo/Demo1Activity.java
@@ -0,0 +1,11 @@
+package com.example.wolf.myanimationdemo;
+
+import android.animation.Animator;
+import android.app.Activity;
+
+/**
+ * Created by wolf on 15/10/26.
+ */
+public class Demo1Activity extends Activity {
+    Animator
+}
--- AnimationSkillShare/MyAnimationDemo/app/src/main/java/com/example/wolf/myanimationdemo/MainActivity.java
+++ AnimationSkillShare/MyAnimationDemo/app/src/main/java/com/example/wolf/myanimationdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wolf.myanimationdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.anim1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent(MainActivity.this, Demo1Activity.class);
             }
         });
     }
